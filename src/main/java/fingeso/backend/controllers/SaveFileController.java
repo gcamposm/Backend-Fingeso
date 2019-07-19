a7b9dfd179b80ff84a294e3c4d2aa6862c23f6d9
Guillermo Campos <guillermo.campos@usach.cl>
2019-07-19 01:19:59 -0400

    Probando nueva descarga de archivos para server

--- src/main/java/fingeso/backend/controllers/SaveFileController.java
+++ src/main/java/fingeso/backend/controllers/SaveFileController.java
@@ -3,7 +3,11 @@ package fingeso.backend.controllers;
 import fingeso.backend.dao.ProposalDao;
 import fingeso.backend.models.Proposal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,7 +32,10 @@ public class SaveFileController {
         String relativeWebPath = "WEB-INF/classes/static";
         String serverPath = context.getRealPath(relativeWebPath);
         //LOCAL
+
+        //FRONT
         //String absoluteFilePath = "../Symbiose-Front/public/static/";
+        //BACK
         //String absoluteFilePath = "src/main/resources/static/";
 
         Proposal proposal = proposalDao.findProposalByIdStr(proposalId);
@@ -45,10 +52,34 @@ public class SaveFileController {
         return ResponseEntity.ok(proposalDao.save(proposal));
         //return ResponseEntity.ok(serverPath);
     }
-    @RequestMapping(value = "/getfile", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @RequestMapping(value = "/downloadFile", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> downloadFile(@RequestParam("fileName") String fileName) throws IOException
     {
         String absoluteFilePath = "/static/" + fileName;
         return ResponseEntity.ok(absoluteFilePath);
+    }
+
+    ///DOWNLOAD FILE IN THE SERVER
+
+    @RequestMapping(value = "/getfile", method = RequestMethod.GET, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @CrossOrigin
+    @ResponseBody
+    public ResponseEntity<InputStreamResource> getFile(@RequestParam("fileName") String fileName) throws FileNotFoundException {
+        //System.out.println("en get file");
+        MediaType mediaType = MediaType.parseMediaType("application/vnd.ms-excel");
+        File file = new File(fileName); //the fileUtils is org.apache.commons.io.FileUtils;
+        //FileUtils.writeByteArrayToFile(file, fileOptional.get()); // Hope that your get return a byte[] array
+        //System.out.println(file.getAbsolutePath());
+
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        return ResponseEntity.ok()
+                // Content-Disposition
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + file.getName())
+                // Content-Type
+                .contentType(mediaType)
+                // Contet-Length
+                .contentLength(file.length()) //
+                .body(resource);
     }
 }
